edc0a71605f00d50848a81f7f1c5ec7129369dc9
513923907@qq.com <513923907@qq.com>
2023-07-24 18:50:05 +0800

    update product buy 3

--- src/com/lgd/service/impl/OrderServiceImpl.java
+++ src/com/lgd/service/impl/OrderServiceImpl.java
@@ -3,22 +3,24 @@ package com.lgd.service.impl;
 import com.lgd.bean.Order;
 import com.lgd.service.OrderService;
 import com.lgd.utils.BusinessException;
+import com.lgd.utils.OrderIO;
 
 import java.util.List;
 
 public class OrderServiceImpl implements OrderService {
+    private OrderIO orderIO=new OrderIO();
     @Override
     public void buyProduct(Order o) throws BusinessException {
-
+        orderIO.add(o);
     }
 
     @Override
     public List<Order> list() throws BusinessException {
-        return null;
+        return orderIO.list();
     }
 
     @Override
     public Order findById(int orderId) throws BusinessException {
-        return null;
+        return orderIO.findByOrderId(orderId);
     }
 }
